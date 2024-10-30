d9b8ba9e2a1d11320bb072926ca6b73842b4a4b8
André Vital <andre@andrevital.com>
2024-10-30 13:11:32 -0600

    Update build files

--- dist/fingerprint.js
+++ dist/fingerprint.js
@@ -4,25 +4,20 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-
 var _envHelper = _interopRequireDefault(require("./envHelper"));
-
 var _fingerprint = _interopRequireDefault(require("./fingerprint.lib"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 /* eslint-disable no-undef */
-var HASH_KEY = "E86E2612010258B35137";
+
+const HASH_KEY = "E86E2612010258B35137";
+
 /**
  * Function to get browser finger print
  * @returns
  */
-
-var getFingerprint = function getFingerprint() {
-  var HASH_KEY_CUSTOM = _envHelper.default.getHashKey() || HASH_KEY;
+const getFingerprint = () => {
+  let HASH_KEY_CUSTOM = _envHelper.default.getHashKey() || HASH_KEY;
   if (typeof window === "undefined") return HASH_KEY_CUSTOM;
   return _fingerprint.default.getFingerprint() + HASH_KEY_CUSTOM;
 };
-
-var _default = getFingerprint;
-exports.default = _default;
+var _default = exports.default = getFingerprint;
--- dist/utils.js
+++ dist/utils.js
@@ -4,23 +4,19 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getSecurePrefix = exports.getDisabledKeys = exports.FINGERPRINT_KEYS = void 0;
-
 var _envHelper = _interopRequireDefault(require("./envHelper"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 /**
  * Function which is used to get the secure prefix
  * @returns
  */
-var getSecurePrefix = function getSecurePrefix() {
-  var KEY_PREFIX = _envHelper.default.getStoragePrefix() || "@secure.";
+const getSecurePrefix = () => {
+  let KEY_PREFIX = _envHelper.default.getStoragePrefix() || "@secure.";
   if (!KEY_PREFIX.endsWith(".")) return KEY_PREFIX + ".";
   return KEY_PREFIX;
 };
-
 exports.getSecurePrefix = getSecurePrefix;
-var FINGERPRINT_KEYS = {
+const FINGERPRINT_KEYS = exports.FINGERPRINT_KEYS = {
   USERAGENT: "UserAgent",
   SCREEN_PRINT: "ScreenPrint",
   PLUGINS: "Plugins",
@@ -34,22 +30,19 @@ var FINGERPRINT_KEYS = {
   CANVAS: "Canvas",
   HOSTNAME: "Hostname"
 };
+
 /**
  * Function which is used to get all the disabled keys
  * @returns
  */
-
-exports.FINGERPRINT_KEYS = FINGERPRINT_KEYS;
-
-var getDisabledKeys = function getDisabledKeys() {
-  var DISABLED_KEYS = _envHelper.default.getDisabledKeys() || "";
+const getDisabledKeys = () => {
+  let DISABLED_KEYS = _envHelper.default.getDisabledKeys() || "";
   if (DISABLED_KEYS === "") return [];
-  var allOptions = [FINGERPRINT_KEYS.USERAGENT, FINGERPRINT_KEYS.SCREEN_PRINT, FINGERPRINT_KEYS.PLUGINS, FINGERPRINT_KEYS.FONTS, FINGERPRINT_KEYS.LOCAL_STORAGE, FINGERPRINT_KEYS.SESSION_STORAGE, FINGERPRINT_KEYS.TIMEZONE, FINGERPRINT_KEYS.LANGUAGE, FINGERPRINT_KEYS.SYSTEM_LANGUAGE, FINGERPRINT_KEYS.COOKIE, FINGERPRINT_KEYS.CANVAS, FINGERPRINT_KEYS.HOSTNAME];
-  var response = [];
-  DISABLED_KEYS.split("|").forEach(function (key) {
-    if (key === "") {} else if (allOptions.includes(key)) response.push(key);else console.warn("react-secure-storage : ".concat(key, " is not present in the available disabled keys options! Please go through the documentation"));
+  const allOptions = [FINGERPRINT_KEYS.USERAGENT, FINGERPRINT_KEYS.SCREEN_PRINT, FINGERPRINT_KEYS.PLUGINS, FINGERPRINT_KEYS.FONTS, FINGERPRINT_KEYS.LOCAL_STORAGE, FINGERPRINT_KEYS.SESSION_STORAGE, FINGERPRINT_KEYS.TIMEZONE, FINGERPRINT_KEYS.LANGUAGE, FINGERPRINT_KEYS.SYSTEM_LANGUAGE, FINGERPRINT_KEYS.COOKIE, FINGERPRINT_KEYS.CANVAS, FINGERPRINT_KEYS.HOSTNAME];
+  const response = [];
+  DISABLED_KEYS.split("|").forEach(key => {
+    if (key === "") {} else if (allOptions.includes(key)) response.push(key);else console.warn(`react-secure-storage : ${key} is not present in the available disabled keys options! Please go through the documentation`);
   });
   return response;
 };
-
 exports.getDisabledKeys = getDisabledKeys;
